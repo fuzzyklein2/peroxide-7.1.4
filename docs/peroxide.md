75968f7fa9a161280d6180d195bec529a76a5992
Fuzzy Klein <fuzzyklein2@gmail.com>
2026-09-26 13:30:30 -0500

    Build lab/

---
+++ docs/peroxide.md
@@ -0,0 +1,9 @@
+# 🌿 PEROXIDE
+
+Customized audio looper that responds to MIDI messages.
+
+## Synopsis
+
+```bash
+cargo run
+```
--- src/peroxide.rs
+++ src/peroxide.rs
@@ -1,7 +1,14 @@
+//! # 
+// TODO: Get this file to run and run it with -h ==> SYNOPSIS
+// TODO: Write some unit tests
+// #[cfg(test)]
+// mod tests;
+
+use std::cmp::max;
 use std::collections::{ HashMap, VecDeque };
 use std::ffi::{ CString, OsString };
 use std::fs;
-use std::io::{ self, Error, stdout };
+use std::io::{ self, Error, ErrorKind, stdout };
 use std::path::{ Path, PathBuf };
 use std::sync::{ mpsc::{ channel, Sender, Receiver }, Mutex, OnceLock };
 use std::thread;
@@ -40,7 +47,8 @@ pub fn get_most_recent_song_list() -> Result<Vec<String>, io::Error> {
     let lists_dir = session_folder()?.join("lists");
     let mut dir_content: Vec<_> = fs::read_dir(lists_dir)?
         .collect::<Result<Vec<_>, _>>()?;
-    if lists_dir.empty() {
+    // TODO: Find or write a function to determine if a directory is empty.
+    if dir_content.is_empty() {
         error("Song lists directory is empty!");
     }
     let n = dir_content.len();
@@ -56,6 +64,19 @@ trait FromFile: Sized {
         Self: Sized;
 }
 
+trait FromString: Sized {
+    fn from_string(s: &str) -> Result<Self, Error>
+    where
+        Self: Sized;
+}
+
+trait FromJsonValue: Sized {
+    fn from_json_value(js: &JsonValue) -> Result<Self, Error>
+    where
+        Self: Sized;
+}
+
+#[derive(Debug)]
 pub struct Clip {
     samples: Vec<f32>,
     frames: usize,
@@ -97,7 +118,7 @@ impl FromFile for Clip {
         
         if result != 0 {
             error("Can't load audio clip!");
-            Err()
+            Err(Error::new(ErrorKind::Other, "Cant load audio clip!"))
         }
         else {
             Ok(Self {
@@ -112,18 +133,42 @@ impl FromFile for Clip {
 
 #[derive(Debug)]
 pub struct Pattern {
+    repeat_count: ,
     value: JsonValue,
 }
 
 impl FromFile for Pattern {
     fn from_file(p: impl AsRef<Path>) -> Result<Self, Error> {
         let contents = fs::read_to_string(p).unwrap();
+        // TODO: Test using an Option as Pattern::repeat_count
+        Ok(Pattern::from_string(&contents)?)
+    } // from_file
+} // impl
 
-        Ok(Self {
-            value: json::parse(&contents).unwrap(),
-        })
-    }
-}
+impl FromString for Pattern {
+    fn from_string(s: &str) -> Result<Self, Error> {
+        let mut js = json::parse(&s).unwrap();
+        let mut repeat_count = 1;
+        for i in 0..js.len() {
+            match js[i] {
+                JsonValue::Number(n) => {
+                    repeat_count = n.as_fixed_point_i64(0).unwrap() as u16;
+                    if repeat_count == 0 { repeat_count = 10000; }
+                    break;
+                } // Number
+                _ => { error("Bad pattern element"); }
+            } // match
+        } // for i
+        Ok(
+            Self {
+                repeat_count,
+                value: js,
+            } // Self
+        ) // Ok
+    } // from_string
+} // impl
+
+// TODO: Implement Pattern::from_json_value
 
 // pub struct Song {
 //     pattern: Pattern,
@@ -132,6 +177,8 @@ impl FromFile for Pattern {
 // pub struct SongList {
 //     songs: Vec<Song>,
 // }
+
+// TODO: Separate Point, Label, etc. to a new crate, `terminal.rs`
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Point {
@@ -182,8 +229,8 @@ impl Label {
 
     pub fn print(&mut self) {
         print!("{}", self.text);
-    }
-}
+    } // print
+} // Label
 
 impl Refresh for Label {
     fn refresh(&mut self) -> Result<(), Error> {
@@ -191,29 +238,35 @@ impl Refresh for Label {
         execute!( stdout(),
                   MoveTo(self.position.x, self.position.y),
                   PrintStyledContent(self.text.clone().with(self.color)),
-                )?;
+                )?; // execute
         Ok(())
-    }
-}
+    } // refresh
+} // impl
 
 #[derive(Debug)]
 enum PlayerEvent {
     Pedal,
     Space,
-}
+    Escape,
+    Rewind,
+    Next,
+} // PlayerEvent
 
-#[derive(Debug)]
+
 pub struct Player {
+    // Debug is not implemented for MidiInputConnection!
     clips: VecDeque<String>,
-    cache: HashMap<String, Clip>,
+    cache: HashMap<OsString, Clip>,
     playing: bool,
     pedal: bool,
     sender: Sender<PlayerEvent>,
     receiver: Receiver<PlayerEvent>,
-    midi_connection: MidiInputConnection,
+    midi_connection: Option<MidiInputConnection<()>>,
     song_list: Vec<String>,
     songs_folder: PathBuf,
-}
+    rewind: bool,
+    next: bool,
+} // Player
 
 impl Player {
     pub fn new () -> Self {
@@ -226,48 +279,47 @@ impl Player {
             pedal: false,
             sender,
             receiver,
-            midi_connection: start_midi(),
-        }
-    }
+            midi_connection: None,
+            song_list: Vec::new(),
+            songs_folder: PathBuf::new(),
+            rewind: false,
+            next: false
+        } // Self
+    } // new
 
     pub fn init(&mut self) -> Result<(), Error> {
-        self.sender = sender;
-        self.receiver = receiver;
         self.get_song_list()?;
         debug(&format!("Searching for the `songs` folder..."));
         self.songs_folder = session_folder()?.join(SONGS_DIR_NAME);
         self.get_keyboard_events();
         self.start_midi();
         Ok(())
-    }
+    } // init
 
     pub fn get_keyboard_events(&mut self) {
         let sender = self.sender.clone();
         thread::spawn(move || {
             loop {
                 if event::poll(Duration::from_millis(50)).unwrap() {
-                    match key.code {
-                        KeyCode::Char(' ') => {
-                            let _ = sender.send(PlayerEvent::Space);
-                        }
-                    
-                        KeyCode::Esc => {
-                            let _ = sender.send(PlayerEvent::Escape);
-                        }
-                    
-                        _ => {}
-                    } // match
-                    
-                    // if let Ok(Event::Key(key)) = event::read() {
-                    //     // This will work for now, but we we'll need to distinguish different keys for different possible commands.
-                    //     // Other Event types can just be added to the PlayerEvent and matched to chars.
-                    //     let _ = sender.send(PlayerEvent::Pedal);
+                    if let Ok(Event::Key(key)) = event::read() {
+                        match key.code {
+                            KeyCode::Char(' ') => {
+                                let _ = sender.send(PlayerEvent::Space);
+                            }
+                        
+                            KeyCode::Esc => {
+                                let _ = sender.send(PlayerEvent::Escape);
+                            }
+                        
+                            _ => {}
+                        } // match
+                    } // if let Ok ...
                 } // if event::poll
             } // loop
         }); // thread::spawn
     } // get_keyboard events
     
-    pub fn start_midi(&mut self) -> MidiInputConnection<()> {
+    pub fn start_midi(&mut self) {
         let sender = self.sender.clone();
         let midi = MidiInput::new("peroxide")
             .expect("Couldn't initialize MIDI");
@@ -278,39 +330,39 @@ impl Player {
     
         for (i, port) in ports.iter().enumerate() {
             println!("{}: {}", i, midi.port_name(port).unwrap());
-        }
+        } // for
     
         let port = ports.get(1)
             .expect("MIDI port 1 doesn't exist.");
     
-        let connection = midi.connect(
-            port,
-            "peroxide-input",
-            move |_timestamp, message, _| {
-                match message {
-                    [0xF8] | [0xFE] => {}
-    
-                    [0xB0, 64, value] if *value > 63 => {
-                        let _ = sender.send(PlayerEvent::Pedal);
-                    }
-    
-                    _ => {
-                        println!("MIDI: {:?}", message);
-                    }
-                }
-            },
-            (),
-        ).expect("Couldn't open MIDI port 1");
+        self.midi_connection = Some(
+                midi.connect(
+                port,
+                "peroxide-input",
+                move |_timestamp, message, _| {
+                    match message {
+                        [0xF8] | [0xFE] => {} // ignore
+        
+                        [0xB0, 64, value] if *value > 63 => {
+                            let _ = sender.send(PlayerEvent::Pedal);
+                        } // Pedal up
+        
+                        _ => {
+                            println!("MIDI: {:?}", message);
+                        } // _
+                    } // match
+                }, // move lambda
+                (),
+            ).expect("Couldn't open MIDI port 1")
+        ); // Some
     
         println!("Successfully opened MIDI port 1");
     
-        connection
-    }
+    } // start_midi
 
     pub fn get_song_list(&mut self) -> Result<(), Error> {
         let arguments = &ARGUMENTS.get().unwrap().args;
         let nargs = arguments.len();
-        let mut song_list = Vec::<String>::new();
         if let Some(input) = INPUT.get() {
             self.song_list = input.lines().map(str::to_owned).collect();
         } else if nargs > 0 {
@@ -326,35 +378,61 @@ impl Player {
 
     }
 
-    pub fn parse(&mut self, pattern: JsonValue) -> Result<(), Error> {
+    // pub fn parse(&mut self, pattern: Pattern) -> Result<(), Error> {
+    //     // Wait for the sustain pedal to begin parsing (playing) the pattern
+    //     if !playing {
+    //         while !self.pedal {
+    //             self.poll_events(); 
+    //         } // while
+    //     } // if !playing
+    //     debug("Pedal event received!\nStarting playback...");
+    //     self.pedal = false;
+    //     self.playing = true;
+        
+    //     match pattern.value {
+    //         JsonValue::Array(a) => self.parse_items(&a, pattern.repeat_count.unwrap()),
+    //         _ => {
+    //             error("Pattern must be an array!");
+    //             Ok(())
+    //         } // error
+    //     } // match
+    // } // parse
+    
+    pub fn parse(&mut self, pattern: Pattern) -> Result<(), Error> {
         // Wait for the sustain pedal to begin parsing (playing) the pattern
-
+        if !self.playing {
+            while !self.pedal {
+                self.poll_events(); 
+            } // while
+        } // if !playing
+        debug("Pedal event received!\nStarting playback...");
+        self.pedal = false;
+        self.playing = true;
         
-        
-        match pattern {
-            JsonValue::Array(a) => self.parse_items(&a),
+        match pattern.value {
+            JsonValue::Array(a) => self.parse_items(&a), &pattern.repeat_count),
             _ => {
                 error("Pattern must be an array!");
                 Ok(())
-            }
-        }
-    }
-    
+            } // error
+        } // match
+    } // parse
+
     /// Parse a pattern recursively
-    pub fn parse_items(&mut self, pattern: &[JsonValue]) -> Result<(), Error> {
+    pub fn parse_items(&mut self, pattern: &Pattern, repeat_count: &u16) -> Result<(), Error> {
         // ...
         debug(&format!("Parsing pattern: {:?}", pattern));
-        let mut repeat_count = 1;
         let mut iteration = 0;
+        if *repeat_count == 0 { repeat_count = 10000; }
 
-        while iteration < repeat_count {
+        while iteration < pattern.repeat_count {
             for item in pattern {
                 match item {
                     JsonValue::Number(n) => {
-                        repeat_count = n.as_fixed_point_i64(0).unwrap() as usize;
+                        // repeat_count = n.as_fixed_point_i64(0).unwrap() as usize;
                         // if repeat_count == 0 { repeat_count = usize::MAX; }
                     } // Number
-                    JsonValue::Array(a) => { self.parse_items(&a)?; }
+                    JsonValue::Array(a) => { self.parse_items(&a,)?; }
                     JsonValue::Short(s) => { 
                         debug(&format!("Clip: {:?}", s));
                         self.clips.push_back(s.to_string());
@@ -363,9 +441,11 @@ impl Player {
                         error("Parsing error!");
                     } // _ (error)
                 } // match
-                iteration += 1;
             } // for 
-            if repeat_count == 0 && self.pedal {
+            iteration += 1;
+            // Finite repetitions need to actually be dealt with soon
+            // This is probably where we need to pause and let the audio finish
+            if self.pedal {
                 self.pedal = false;
                 break;
             } // if
@@ -373,33 +453,47 @@ impl Player {
         Ok(())
     } // parse_items
 
-    pub fun play(&self) -> Result<(), Error> {
-        self.init();
+    fn poll_events(&mut self) {
+        while let Ok(event) = self.receiver.try_recv() {
+            match event {
+                PlayerEvent::Pedal => self.pedal = true,
+                PlayerEvent::Space => self.pedal = true,
+                PlayerEvent::Escape => self.playing = false,
+                PlayerEvent::Rewind => self.rewind = true,
+                PlayerEvent::Next => self.next = true,
+            } // match
+        } // while
+    } // poll_events
+    
+    pub fn play(&mut self) -> Result<(), Error> {
+        self.init()?;
+        
         for song_title in self.song_list {
+            info(&format!("Song: {}", song_title));
             let song_folder = self.songs_folder.join(song_title);
             let clips_folder = song_folder.join(CLIPS_DIR_NAME);
 
-            let mut files: Vec<_> = fs::read_dir(clips_folder)?
+            let files: Vec<_> = fs::read_dir(clips_folder)?
                 .collect::<Result<Vec<_>, _>>()?;
 
             for f in files {
                 let key: OsString = f.path().file_stem().expect("REASON").to_owned();
-
                 match Clip::from_file(f.path()) {
                     Ok(clip) => {
-                        clips_map.insert(key, clip); 
+                        self.cache.insert(key, clip); 
                         info(&format!("Clip loaded: {:?}", key));            
                     } // Ok
                     Err(e) => {
-                        error(&format!("Clip could not be loaded from file: {:?}", key)); 
+                        error(&format!("Clip could not be loaded from file: {:?}: {}", key, e)); 
                     } // Err
                 } // match
-
-                let song_file = song_folder.join(SONG_FILE_NAME);
-                let contents = fs::read_to_string(song_file).unwrap();
-                let js = json::parse(&contents).unwrap();
-                self.parse(js);
             } // for f
+            let song_file = song_folder.join(SONG_FILE_NAME);
+            let contents = fs::read_to_string(song_file)?;
+            match json::parse(&contents) {
+                Ok(js) => { self.parse(&Pattern::from_json_value(js))?; }
+                Err(e) => { error("Error parsing JSON pattern!"); }
+            } // match
         } // for song_title
         
         Ok(())
@@ -408,9 +502,9 @@ impl Player {
 
 
 pub fn run() -> Result<(), Error> {
-    let player = Player::new();
+    let mut player = Player::new();
     player.play();
-    
+    // TODO: Implement the curses style interface in the comment below.
     Ok(())
 }
 
